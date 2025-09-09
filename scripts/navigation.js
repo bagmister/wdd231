8cdf1ff0188bf0f0eeae425560221ecb3056294b
kevin bagley <bagmister@yahoo.com>
2025-09-08 22:37:58 -0600

    week01 updates

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,9 @@
 const navButton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
+const h1 = document.querySelector('#nav-header-text')
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
   navBar.classList.toggle('show');
+  h1.textContent
 });
